fd1f9bf72a7cc89cc9aecd332f23b583608febdb
mxs <1721748881@qq.com>
2024-04-03 14:24:13 +0800

    add common package add constant package add exception package add config package

---
+++ backend/src/main/java/com/chinasoft/backend/exception/BusinessException.java
@@ -0,0 +1,33 @@
+package com.chinasoft.backend.exception;
+
+
+import com.chinasoft.backend.common.ErrorCode;
+
+/**
+ * 业务异常类
+ *
+ * @author mxs
+ */
+public class BusinessException extends RuntimeException {
+
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
---
+++ backend/src/main/java/com/chinasoft/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.chinasoft.backend.exception;
+
+import com.chinasoft.backend.common.BaseResponse;
+import com.chinasoft.backend.common.ErrorCode;
+import com.chinasoft.backend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author mxs
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
