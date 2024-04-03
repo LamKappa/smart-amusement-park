7c74efbcf84cf6c6f5410b8657832316a2c20575
mxs <1721748881@qq.com>
2024-04-03 10:13:14 +0800

    fix

--- backend/src/main/java/com/chinasoft/backend/BackendApplication.java
+++ backend/src/main/java/com/chinasoft/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.chinasoft.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
--- backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.chinasoft.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
