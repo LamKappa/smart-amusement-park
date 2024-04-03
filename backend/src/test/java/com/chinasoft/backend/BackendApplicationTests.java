c334d9c2b82e4a40f34b2c8ffc1deff2d56dc1b4
mxs <1721748881@qq.com>
2024-04-03 14:46:16 +0800

    add User add UserService add UserMapper

--- backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
@@ -1,14 +1,24 @@
 package com.chinasoft.backend;
 
+import com.chinasoft.backend.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    UserService userService;
+
     @Test
     void contextLoads() {
-        
+
+    }
+
+    @Test
+    void testUser() {
+        System.out.println(userService.list());
     }
 
 }
