67e8343fd08c14e7024e577d0986b36f5421cab1
mxs <1721748881@qq.com>
2024-04-03 15:02:26 +0800

    add AmusementFacility add AmusementFacilityService add AmusementFacilityMapper

--- backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/chinasoft/backend/BackendApplicationTests.java
@@ -1,15 +1,22 @@
 package com.chinasoft.backend;
 
+import com.chinasoft.backend.model.entity.AmusementFacility;
+import com.chinasoft.backend.service.AmusementFacilityService;
 import com.chinasoft.backend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    AmusementFacilityService amusementFacilityService;
 
     @Test
     void contextLoads() {
@@ -19,6 +26,13 @@ class BackendApplicationTests {
     @Test
     void testUser() {
         System.out.println(userService.list());
+    }
+
+    @Test
+    void testAmusementFacility() {
+        List<AmusementFacility> list = amusementFacilityService.list();
+        System.out.println(list);
+
     }
 
 }
