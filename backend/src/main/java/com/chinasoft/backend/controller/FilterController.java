a078121f7b99c47cc4b939dfe229f283a82ca64e
皎皎 <809905706@qq.com>
2024-04-08 23:16:33 +0800

    增加单点导航

--- backend/src/main/java/com/chinasoft/backend/controller/FilterController.java
+++ backend/src/main/java/com/chinasoft/backend/controller/FilterController.java
@@ -14,14 +14,12 @@ import com.chinasoft.backend.service.AmusementFacilityService;
 import com.chinasoft.backend.service.BaseFacilityService;
 import com.chinasoft.backend.service.RestaurantFacilityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/filter")
 public class FilterController {
 
