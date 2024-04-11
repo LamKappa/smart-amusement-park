70eb5eb75402de81d9e6492610dd36b69bb45972
皎皎 <809905706@qq.com>
2024-04-11 14:59:56 +0800

    完成各个设施人数统计

---
+++ backend/src/main/java/com/chinasoft/backend/mapper/FacilityHeadcountMapper.java
@@ -0,0 +1,18 @@
+package com.chinasoft.backend.mapper;
+
+import com.chinasoft.backend.model.entity.FacilityHeadcount;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 皎皎
+* @description 针对表【facility_headcount(各个游玩设施人数统计表)】的数据库操作Mapper
+* @createDate 2024-04-11 14:13:04
+* @Entity com.chinasoft.backend.model.entity.FacilityHeadcount
+*/
+public interface FacilityHeadcountMapper extends BaseMapper<FacilityHeadcount> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/com/chinasoft/backend/service/impl/FacilityHeadcountServiceImpl.java
@@ -0,0 +1,22 @@
+package com.chinasoft.backend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chinasoft.backend.model.entity.FacilityHeadcount;
+import com.chinasoft.backend.service.FacilityHeadcountService;
+import com.chinasoft.backend.mapper.FacilityHeadcountMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 皎皎
+* @description 针对表【facility_headcount(各个游玩设施人数统计表)】的数据库操作Service实现
+* @createDate 2024-04-11 14:13:04
+*/
+@Service
+public class FacilityHeadcountServiceImpl extends ServiceImpl<FacilityHeadcountMapper, FacilityHeadcount>
+    implements FacilityHeadcountService{
+
+}
+
+
+
+
